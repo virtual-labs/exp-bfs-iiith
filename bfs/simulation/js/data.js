24ae33d1e3812dc1117e860e4b5dff2c97c095fa
vleadadmin <admin@vlabs.ac.in>
2021-04-21 16:05:58 +0000

    https://virtual-labs.github.io/virtual-labs/exp-bfs-iiith click on the link to test your code.

---
+++ bfs/simulation/js/data.js
@@ -0,0 +1,34 @@
+    var graphs = [
+    function(){
+        g.addNode("1");
+        g.addNode("2");
+        g.addNode("3");
+        g.addNode("4");
+        g.addNode("5");
+        g.addEdge("1", "2", 8);
+        g.addEdge("1", "3", 9);
+        g.addEdge("1", "4", 10);
+        g.addEdge("2", "3", 11);
+        g.addEdge("2", "5", 15);
+    }
+    
+    ];
+
+var a=[[[{ id: 1, label: ' 0 ' },
+        { id: 2, label: ' 1 ' },
+        { id: 3, label: ' 2 ' },
+        { id: 4, label: ' 3 ' },
+        { id: 5, label: ' 4 ' },
+
+    
+    ],
+    [
+    { from: 1, to: 2, id: "8" },
+        { from: 1, to: 3, id: "9" },
+        { from: 1, to: 4, id: "10" },
+        { from: 2, to: 3, id: "11" },
+        { from: 2, to: 5, id: "15" },
+    
+    ]
+    ]
+  ];
