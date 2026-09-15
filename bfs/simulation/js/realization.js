2a916235b981a29955ff84cc3d7b11a2666b11ab
vleadadmin <admin@vlabs.ac.in>
2026-09-15 13:33:52 +0000

    https://virtual-labs.github.io/virtual-labs/exp-bfs-iiith click on the link to test your code.

---
+++ bfs/simulation/js/realization.js
@@ -0,0 +1,99 @@
+var realizationCatalog = {
+  // Video Artefacts
+  "BFS Introduction": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/0MYW7JymfMc"
+  },
+
+  "BFS algorithm": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/ivW2JAF9-Tw"
+  },
+
+  "BFS Concept": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/fYXwV7e_vlc"
+  },
+
+  "Time and Space Complexity of BFS": {
+    "resource_type": "video",
+    "url": "https://www.youtube.com/embed/Jf2vNh9fNVc"
+  },
+
+  // Image Artefacts
+  "Graph Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/graph.png"
+  },
+
+  "Undirected Graph Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/undirected.png"
+  },
+
+  "Directed Graph Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/directed.png"
+  },
+
+  "Queues Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/queue/queue.png"
+  },
+  
+  "Enqueue and Dequeue Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/queue.gif"
+  },
+
+  "Circular Queue Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/circular\ queue.gif"
+  },
+  
+  "BFS Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/bfs-concept/BFS.png"
+  },
+
+  "BFS Algorithm Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/bfs-algo/bfs_steps.png"
+  },
+
+  "Pictorial Representation of Graph": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/graph.png"
+  },
+  
+  "BFS Travel Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/bfs.png"
+  },
+  
+  "DFS Traversal Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/dfs.png"
+  },
+
+  // Html Artefacts
+  "BFS : Demo (Graphs)": {
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bfs_demo.html"
+  },
+
+  "BFS : Practice (Graphs)": {
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bfs_practice.html"
+  },
+
+  "BFS : Practice (Queue)": {
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bfs_using_queue.html"
+  },
+
+  "BFS Exercise": {
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/bfs_exercise.html"
+  }
+};
